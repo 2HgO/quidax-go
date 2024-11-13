b2a95f78b4d9955c59036122132635d735421625
Oghogho Odemwingie <odemwingieog@gmail.com>
2024-11-13 04:39:25 +0100

    feat: slim down replica set to single node

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[ ! -f /data/$CLUSTERNAME.tigerbeetle ] && /tigerbeetle format --cluster=0 --replica=$CLUSTERNUMBER --replica-count=3 /data/$CLUSTERNAME.tigerbeetle;
+[ ! -f /data/$CLUSTERNAME.tigerbeetle ] && /tigerbeetle format --cluster=0 --replica=$CLUSTERNUMBER --replica-count=$CLUSTERCOUNT /data/$CLUSTERNAME.tigerbeetle;
 
 /tigerbeetle "$@";
