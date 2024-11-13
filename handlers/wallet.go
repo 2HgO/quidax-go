fa17942cecb903e090839189cad47338b61430c0
Oghogho Odemwingie <odemwingieog@gmail.com>
2024-11-13 03:16:01 +0100

    chore

--- handlers/wallet.go
+++ handlers/wallet.go
@@ -6,6 +6,7 @@ import (
 	"github.com/2HgO/quidax-go/errors"
 	"github.com/2HgO/quidax-go/services"
 	"github.com/2HgO/quidax-go/types/requests"
+	"github.com/2HgO/quidax-go/types/responses"
 	"github.com/2HgO/quidax-go/utils"
 	"go.uber.org/zap"
 )
@@ -30,6 +31,26 @@ type walletHandler struct {
 func (ws *walletHandler) ServeHttp(mux *http.ServeMux) {
 	mux.HandleFunc("GET /api/v1/users/{user_id}/wallets", ws.middlewares.AttchValidateAccessToken(ws.FetchUserWallets))
 	mux.HandleFunc("GET /api/v1/users/{user_id}/wallets/{currency}", ws.middlewares.AttchValidateAccessToken(ws.FetchUserWallet))
+	mux.HandleFunc("GET /api/v1/users/{user_id}/wallets/{currency}/address", ws.middlewares.AttchValidateAccessToken(func(w http.ResponseWriter, r *http.Request) {
+		walletRes, err := ws.walletService.FetchUserWallet(r.Context(), &requests.FetchUserWalletRequest{UserID: r.PathValue("user_id"), Currency: r.PathValue("currency")})
+		if err != nil {
+			errors.AsAppError(err).Serialize(w)
+			return
+		}
+		wallet := walletRes.Data
+		utils.JSON(w, 200, responses.Response[any]{
+			Status: "successful",
+			Data: map[string]any{
+				"id":              wallet.ID,
+				"reference":       wallet.ID,
+				"currency":        wallet.Currency,
+				"address":         "",
+				"destination_tag": "deposit_not_supported",
+				"total_payments":  "0",
+				"network":         "",
+			},
+		})
+	}))
 }
 
 func (ws *walletHandler) FetchUserWallet(w http.ResponseWriter, r *http.Request) {
